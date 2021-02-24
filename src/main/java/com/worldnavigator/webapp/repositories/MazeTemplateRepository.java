c9a307db478a57454d105a254d5e1dc432db5a69
Anwaar <anwaar95omar@gmail.com>
2021-02-25 00:38:58 +0200

    controllers

--- src/main/java/com/worldnavigator/webapp/repositories/MazeTemplateRepository.java
+++
@@ -1,8 +0,0 @@
-package com.worldnavigator.webapp.repositories;
-
-import com.worldnavigator.webapp.entities.MazeTemplate;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MazeTemplateRepository extends JpaRepository<MazeTemplate, Long> {}
--- src/main/java/com/worldnavigator/webapp/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.worldnavigator.webapp.repositories;
-
-import com.worldnavigator.webapp.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, String> {}
